c93d53845ab194acc8141b53ed03240362fdf874
KiaMasoudifar <145088582+KiaMasoudifar@users.noreply.github.com>
2024-06-04 20:08:08 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let paragraphElement = document.querySelector('p');
+
+function changeParagraphText() {
+    paragraphElement.textContent = "Clicked!"
+    
+}
+
+paragraphElement.addEventListener('click', changeParagraphText)
